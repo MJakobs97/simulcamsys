0f7fd48aae645dc6809acd89646c5548d6a6aedd
mjakobs2 <markus.jakobs@smail.th-koeln.de>
2021-09-18 17:51:46 +0100

    iterative ble routine added

--- ble_find_and_connect.sh
+++ ble_find_and_connect.sh
@@ -12,5 +12,18 @@ sleep 5 && echo "finished scanning"
 #grep out the mac address from "Device [MacAddress] [Identifier]" and store in file
 grep -iEo '([[:space:]][a-fA-F0-9]{2})([ :-]([a-fA-F0-9]{2})){5}' tmp_storage/found_devices.txt >> tmp_storage/isolated_mac.txt
 
+echo "trust & pairing - routine running"
+let counter=0
 
+#iterate over all MAC's to trust, pair & connect devices
+while IFS= read -r line; do 
+	echo $counter
+	bluetoothctl -- trust $line & > /dev/null
+	sleep 5
+	bluetoothctl -- paur $line & > /dev/null
+	sleep 5
+	bluetoothctl -- connect $line & > /dev/null
+	let counter++
+done < tmp_storage/isolated_mac.txt
 
+echo "$counter devices paired"
