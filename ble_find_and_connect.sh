a8fecf6dc8867f798dd5acd8c5179b779d68b851
mjakobs2 <markus.jakobs@smail.th-koeln.de>
2021-09-18 17:46:56 +0100

    added grep with regex to isolate mac from bluetoothctl output

--- ble_find_and_connect.sh
+++ ble_find_and_connect.sh
@@ -8,3 +8,9 @@ echo "ble scan off"
 
 bluetoothctl -- devices >> tmp_storage/found_devices.txt
 sleep 5 && echo "finished scanning"
+
+#grep out the mac address from "Device [MacAddress] [Identifier]" and store in file
+grep -iEo '([[:space:]][a-fA-F0-9]{2})([ :-]([a-fA-F0-9]{2})){5}' tmp_storage/found_devices.txt >> tmp_storage/isolated_mac.txt
+
+
+
