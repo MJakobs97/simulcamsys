e1b0e7ae956c4261ea3756ecc27a314db0eebda1
mjakobs2 <markus.jakobs@smail.th-koeln.de>
2021-12-03 11:40:08 +0100

    deleted junk from smController

--- python version/camfsm/smController.py
+++ python version/camfsm/smController.py
@@ -26,32 +26,3 @@ try:
 
 finally: 
 	pass
-
-
-
-#def on_press(key):
- #       try:
-            ##print('Alphanumeric key pressed: {0} '.format(key.char))
-            ##print('Key that is currently being held down: {0}'.format(key.char))
-  #          if format(key.char) == 'r':
-   #             sm.on_event('dms1')
-                
-    #    except AttributeError:
-     #       print('')
-
-#def on_release(key):
-  #      try:
- #           if format(key.char) == 'r':
-   #             sm.on_event('dms0')
-                
-    #        if key == keyboard.Key.esc:
-                # Stop listener
-     #           return False
-      #  except AttributeError:
-       #     print('')
-
-# Collect events until released
-#with keyboard.Listener(
- #       on_press=on_press,
-  #      on_release=on_release) as listener:
-   # listener.join()
