bd5b056a03dbbb5f4b6f0be4d410afff9b180cd4
MJakobs97 <ubuntu@ubuntu.bast.lan>
2022-04-27 13:56:24 +0000

    added subscribe_status function

--- camfsm/ble_outsourcing.py
+++ camfsm/ble_outsourcing.py
@@ -39,6 +39,29 @@ async def rec_stop(client, address):
      print("Exception in rec_stop: \n", ex)
      sys.exit("Rec_stop failed, please await program restart!")
 
+async def subscribe_status(client, address):
+#send "subscribe" request for push notifications about
+#int.bat% = 70
+#gps_status = 68
+#remaining dskspc = 54
+#to queryID 0x93 = 0x53 + 1001 = 0x93
+    try:
+     await client.write_gatt_char(address, bytearray([0x04,0x93,70,68,54]))
+    except Exception as ex:
+     print("Exception in subscribe_status: \n", ex)
+     sys.exit("Subscription failed")
+
+
+
+
+
+
+
+
+
+
+
+
 async def connect_ble(notification_handler: Callable[[int, bytes], None], identifier: str = None) -> BleakClient:
    	    #DO NOT TOUCH THIS FUNCTION! IT IS WORKING AS EXPECTED!
             
