720c398d3f10f8cd7024922498622fcff593ed46
MJakobs97 <Praktikant_Jakobs@bast.de>
2022-05-18 13:33:17 +0200

    added compare_and_remove() and upload_data() functions

---
+++ camfsm/notification_outsourcing.py
@@ -0,0 +1,24 @@
+from statesimpl import DataRep
+
+def compare_and_remove(dbdata, client_address_order, client_address_read_index, database):
+ if not dbdata.id:
+  dbdata.store(database)
+ dbdata = DataRep.load(database, dbdata.id)
+    
+ if dbdata.data:
+  for i in range(len(dbdata.data)): 
+   print("Looking for: \n", client_address_order[client_address_read_index])
+   if str(client_address_order[client_address_read_index]) == dbdata.data[i].address:
+    dbdata.data.remove(dbdata.data[i])
+    print("Removed: \n", str(dbdata.data[i]))
+       
+def upload_data(clients, client_address_order, handle, QUERY_RSP_UUID, dbdata, response, database):
+ for t in clients:
+  if t.address == client_address_order[client_address_read_index] and t.services.characteristics[handle].uuid == QUERY_RSP_UUID:
+   dbdata.data.append(address = t.address, battery = response.data[70][0], disk = int.from_bytes(response.data[54], "big"), gps= response.data[68][0])
+   dbdata.store(database)
+   if client_address_read_index < (len(client_address_order)-1):
+    client_address_read_index = client_address_read_index +1
+   else:
+    client_address_read_index = 0
+   return client_address_read_index
