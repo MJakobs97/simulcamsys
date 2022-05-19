0b9aa27221e6efdbf79936b451ba4bcc1af72cfb
MJakobs97 <Praktikant_Jakobs@bast.de>
2022-05-19 16:01:17 +0200

    added delay in rec_stop()

--- camfsm/ble_outsourcing.py
+++ camfsm/ble_outsourcing.py
@@ -34,6 +34,7 @@ async def rec_start(client, address):
 
 async def rec_stop(client, address):
     try:
+     await asyncio.sleep(5)
      await client.write_gatt_char(address, bytearray([3, 1, 1, 0]))
     except Exception as ex:
      print("Exception in rec_stop: \n", ex)
