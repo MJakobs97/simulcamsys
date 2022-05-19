f38a0285a62445e87103119b1f3ca938f6317c15
MJakobs97 <Praktikant_Jakobs@bast.de>
2022-05-19 15:26:33 +0200

    added client_address_read_index to signature upload_data()

--- camfsm/notification_outsourcing.py
+++ camfsm/notification_outsourcing.py
@@ -12,7 +12,7 @@ def compare_and_remove(dbdata, client_address_order, client_address_read_index, 
     dbdata.data.remove(dbdata.data[i])
     print("Removed: \n", str(dbdata.data[i]))
        
-def upload_data(clients, client_address_order, handle, QUERY_RSP_UUID, dbdata, response, database):
+def upload_data(clients, client_address_order, client_address_read_index, handle, QUERY_RSP_UUID, dbdata, response, database):
  for t in clients:
   if t.address == client_address_order[client_address_read_index] and t.services.characteristics[handle].uuid == QUERY_RSP_UUID:
    dbdata.data.append(address = t.address, battery = response.data[70][0], disk = int.from_bytes(response.data[54], "big"), gps= response.data[68][0])
