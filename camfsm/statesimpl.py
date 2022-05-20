38fd4f7314ef44ff0e0ec2a28c8b66e070d5351f
MJakobs97 <Praktikant_Jakobs@bast.de>
2022-05-20 09:41:47 +0200

    added loop refresh before run_async_another_thread function

--- camfsm/statesimpl.py
+++ camfsm/statesimpl.py
@@ -132,6 +132,9 @@ class ConnectingState(State):
            global dbdata
            global client_address_read_index
            try:
+            global global_loop
+            compare_loop = global_loop
+            asyncio.set_event_loop(compare_loop)
             asyncio.get_event_loop().run_until_complete(run_async_another_thread(compare_and_remove(dbdata, client_address_order, client_address_read_index, database)))
             """
             if not dbdata.id:
@@ -149,6 +152,9 @@ class ConnectingState(State):
             print("Exception while trying to remove existant entries: \n", ex)
 
            try:
+            global global_loop
+            upload_loop = global_loop
+            asyncio.set_event_loop(upload_loop)
             asyncio.get_event_loop().run_until_complete(run_async_another_thread(upload_data(clients, client_address_order,client_address_read_index, handle, QUERY_RSP_UUID, dbdata, response, database)))
             query_event.set()
             return
